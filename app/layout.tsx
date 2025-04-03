9c90ecb00c10be474e231029689b5cb8dce55462
mdrgoncalves <mdr.goncalves@hotmail.com>
2025-04-03 12:30:35 -0300

    refactor: replace header and navigation components, improving layout structure and responsiveness

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
-import Logo from "@/components/Logo";
-import Navigation from "@/components/Navigation";
-
 import { Josefin_Sans } from "next/font/google";
+
+import Header from "@/components/Header";
 
 const josefinSans = Josefin_Sans({
   subsets: ["latin"],
@@ -9,7 +8,6 @@ const josefinSans = Josefin_Sans({
 });
 
 import "@/styles/globals.css";
-
 export const metadata = {
   title: {
     template: "%s | The Wild Oasis",
@@ -28,14 +26,12 @@ export default function RootLayout({
     <html lang="en">
       <body
         suppressHydrationWarning={true}
-        className={`${josefinSans.className} bg-primary-950 text-primary-100 min-h-screen`}
+        className={`${josefinSans.className} bg-primary-950 text-primary-100 min-h-screen flex flex-col`}
       >
-        <header>
-          <Logo />
-          <Navigation />
-        </header>
-        <main>{children}</main>
-        <footer>Copyright by The Wild Oasis</footer>
+        <Header />
+        <div className="flex-1 px-8 py-12">
+          <main className="max-w-7xl mx-auto">{children}</main>
+        </div>
       </body>
     </html>
   );
