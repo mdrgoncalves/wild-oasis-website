a2d5efad692255270bda893af86ec2adc15f4663
mdrgoncalves <mdr.goncalves@hotmail.com>
2025-05-12 15:39:33 -0300

    feat: add CabinList and Spinner components, update loading states in cabins and main loading pages

---
+++ app/_components/CabinList.tsx
@@ -0,0 +1,18 @@
+import { getCabins } from "../_lib/data-service";
+import CabinCard from "./CabinCard";
+
+import type { CabinsType } from "../types/data-service";
+
+async function CabinList() {
+  const cabins: CabinsType = await getCabins();
+
+  if (!cabins.length) return null;
+
+  <div className="grid sm:grid-cols-1 md:grid-cols-2 gap-8 lg:gap-12 xl:gap-14">
+    {cabins.map((cabin) => (
+      <CabinCard cabin={cabin} key={cabin.id} />
+    ))}
+  </div>;
+}
+
+export default CabinList;
