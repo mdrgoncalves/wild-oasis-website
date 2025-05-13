b034f1b7f34727c5c8dd46b83e3eeec04c0c02ff
mdrgoncalves <mdr.goncalves@hotmail.com>
2025-05-13 11:17:31 -0300

    feat: refactor CabinList component to return JSX correctly and add Cabin page with metadata generation

--- app/_components/CabinList.tsx
+++ app/_components/CabinList.tsx
@@ -8,11 +8,13 @@ async function CabinList() {
 
   if (!cabins.length) return null;
 
-  <div className="grid sm:grid-cols-1 md:grid-cols-2 gap-8 lg:gap-12 xl:gap-14">
-    {cabins.map((cabin) => (
-      <CabinCard cabin={cabin} key={cabin.id} />
-    ))}
-  </div>;
+  return (
+    <div className="grid sm:grid-cols-1 md:grid-cols-2 gap-8 lg:gap-12 xl:gap-14">
+      {cabins.map((cabin) => (
+        <CabinCard cabin={cabin} key={cabin.id} />
+      ))}
+    </div>
+  );
 }
 
 export default CabinList;
