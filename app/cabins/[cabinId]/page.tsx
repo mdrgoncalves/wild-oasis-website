04d8a677ea349df9905c49f1fe6dae1f129f7c18
mdrgoncalves <mdr.goncalves@hotmail.com>
2025-05-19 12:18:03 -0300

    refactor remove TextExpander component to streamline codebase

--- app/cabins/[cabinId]/page.tsx
+++ app/cabins/[cabinId]/page.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import { EyeSlashIcon, MapPinIcon, UsersIcon } from "@heroicons/react/24/solid";
-import { getCabin, getCabins } from "@/app/_lib/data-service";
+import { getCabin, getCabins } from "@/lib/data-service";
+import TextExpander from "@/components/TextExpander";
 
 interface CabinPageProps {
   params: {
@@ -48,7 +49,9 @@ export default async function Page({ params }: CabinPageProps) {
             Cabin {name}
           </h3>
 
-          <p className="text-lg text-primary-300 mb-10">{description}</p>
+          <p className="text-lg text-primary-300 mb-10">
+            <TextExpander>{description}</TextExpander>
+          </p>
 
           <ul className="flex flex-col gap-4 mb-7">
             <li className="flex gap-3 items-center">
