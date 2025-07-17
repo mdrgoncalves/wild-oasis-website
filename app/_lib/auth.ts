8f4bbdda38dbac9e0109563b8e2b4bef90481569
mdrgoncalves <mdr.goncalves@hotmail.com>
2025-07-17 15:11:59 -0300

    feat: add authentication logic for guest users in NextAuth

--- app/_lib/auth.ts
+++ app/_lib/auth.ts
@@ -1,10 +1,21 @@
-import NextAuth, { Session } from "next-auth";
+import NextAuth, { Session, User } from "next-auth";
 import { NextRequest } from "next/server";
 import Google from "next-auth/providers/google";
+
+import { createGuest, getGuest } from "./data-service";
 
 type AuthorizedParams = {
   auth: Session | null;
   request?: NextRequest;
+};
+
+type SessionWithUserId = Session & {
+  user: User & { guestId?: string };
+};
+
+type SessionParams = {
+  session: SessionWithUserId;
+  user: User;
 };
 
 const authConfig = {
@@ -17,6 +28,29 @@ const authConfig = {
   callbacks: {
     authorized({ auth }: AuthorizedParams) {
       return !!auth?.user;
+    },
+    async signIn({ user }: { user: User }) {
+      // This callback can be used to handle sign-in logic
+      try {
+        const existingGuest = await getGuest(user.email);
+
+        if (!existingGuest)
+          await createGuest({
+            email: user.email,
+            fullName: user.name,
+          });
+
+        return true;
+      } catch {
+        return false;
+      }
+    },
+    async session({ session, user }: SessionParams) {
+      // Customize the session object here
+      if (!session.user) session.user = user;
+      const guest = await getGuest(session.user.email);
+      session.user.guestId = guest?.id;
+      return session;
     },
   },
   pages: {
