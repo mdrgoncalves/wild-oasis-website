9e0a61199a351d10a6ac8201737f86f14a6925b8
Michael Douglas Rocha Goncalves <mdr.goncalves@hotmail.com>
2025-09-24 11:30:06 -0300

    feat: refactor UpdateProfileForm to include button component with loading state

--- app/_components/UpdateProfileForm.tsx
+++ app/_components/UpdateProfileForm.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { useFormStatus } from "react-dom";
 import { updateGuest } from "../_lib/actions";
 
 import type { Guest } from "../types/guest";
@@ -13,7 +14,10 @@ function UpdateProfileForm({ guest, children }: UpdateProfileFormProps) {
   const { fullName, email, nationalID, countryFlag } = guest;
 
   return (
-    <form action={updateGuest} className="bg-primary-900 py-8 px-12 text-lg flex gap-6 flex-col">
+    <form
+      action={updateGuest}
+      className="bg-primary-900 py-8 px-12 text-lg flex gap-6 flex-col"
+    >
       <div className="space-y-2">
         <label>Full name</label>
         <input
@@ -57,11 +61,22 @@ function UpdateProfileForm({ guest, children }: UpdateProfileFormProps) {
       </div>
 
       <div className="flex justify-end items-center gap-6">
-        <button className="bg-accent-500 px-8 py-4 text-primary-800 font-semibold hover:bg-accent-600 transition-all disabled:cursor-not-allowed disabled:bg-gray-500 disabled:text-gray-300">
-          Update profile
-        </button>
+        <Button />
       </div>
     </form>
+  );
+}
+
+function Button() {
+  const { pending } = useFormStatus();
+
+  return (
+    <button
+      className="bg-accent-500 px-8 py-4 text-primary-800 font-semibold hover:bg-accent-600 transition-all disabled:cursor-not-allowed disabled:bg-gray-500 disabled:text-gray-300"
+      disabled={pending}
+    >
+      {pending ? "Updating..." : "Update profile"}
+    </button>
   );
 }
 
